6d1198bbcd02a04621a15c56259bce8f6707bf21
Scuervo07 <113921960+Scuervo07@users.noreply.github.com>
2022-11-07 08:58:57 -0500

    Cambios animacion

---
+++ app/src/main/java/com/example/apptender2/view/ui/activities/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.apptender2.view.ui.activities
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.ActionBar
+import com.example.apptender2.R
+import com.example.apptender2.databinding.ActivityMainBinding
+import kotlinx.coroutines.delay
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding:ActivityMainBinding
+    lateinit var handler: Handler
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.animationView.setAnimation(R.raw.fruta
+        )
+        binding.animationView.playAnimation()
+
+        handler = Handler(Looper.myLooper()!!)
+        handler.postDelayed(
+            {
+                val intent = Intent(this, LoginActivity::class.java)
+                startActivity(intent)
+                finish()
+            }, 10000)
+
+
+    }
+
+}

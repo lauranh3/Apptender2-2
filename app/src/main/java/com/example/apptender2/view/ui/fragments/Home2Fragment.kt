bf3f9f93dbf40569a6a4e02682bc9c3a5487f23b
Scuervo07 <113921960+Scuervo07@users.noreply.github.com>
2022-11-07 17:26:00 -0500

    home2

---
+++ app/src/main/java/com/example/apptender2/view/ui/fragments/Home2Fragment.kt
@@ -0,0 +1,22 @@
+package com.example.apptender2.view.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.apptender2.R
+
+
+class Home2Fragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_home2, container, false)
+
+        return view
+    
+    }
+}

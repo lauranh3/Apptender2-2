ea98e3f5632b85d124a05dad4cf20abf809c1d59
Scuervo07 <113921960+Scuervo07@users.noreply.github.com>
2022-11-08 21:18:15 -0500

    Conexion Home a  Home2

--- app/src/main/java/com/example/apptender2/view/ui/fragments/BananosFragment.kt
+++ app/src/main/java/com/example/apptender2/view/ui/fragments/BananosFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.navigation.fragment.findNavController
 import com.example.apptender2.R
 
 class BananosFragment : Fragment() {
@@ -13,9 +15,19 @@ class BananosFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_bananos, container, false)
+        val view = inflater.inflate(R.layout.fragment_bananos, container, false)
+        return view
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        val cardBanana = view.findViewById<ImageView>(R.id.Banano)
+        cardBanana.setOnClickListener {
+            findNavController().navigate(R.id.action_bananosFragment_to_homeFragment)
+
+
+        }
+
+    }
 }
--- app/src/main/java/com/example/apptender2/view/ui/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/apptender2/view/ui/fragments/HomeFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.navigation.fragment.findNavController
 import com.example.apptender2.R
 
 
@@ -14,9 +16,25 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+        return view
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        val cardBanana = view.findViewById<ImageView>(R.id.Banano)
+        cardBanana.setOnClickListener {
+            findNavController().navigate(R.id.action_homeFragment_to_bananosFragment)
+
+            val cardFresa = view.findViewById<ImageView>(R.id.Fresas)
+            cardFresa.setOnClickListener {
+                findNavController().navigate(R.id.action_homeFragment_to_fresasFragment)
+            }
+
+
+        }
+
+
+    }
 }

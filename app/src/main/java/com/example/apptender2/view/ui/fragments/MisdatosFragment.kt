c21c82b1ef35d4399f0013fed91be9c8d89b3d24
Scuervo07 <113921960+Scuervo07@users.noreply.github.com>
2022-11-13 17:07:26 -0500

    "Mis datos_perfil_camara_galeria"

--- app/src/main/java/com/example/apptender2/view/ui/fragments/MisdatosFragment.kt
+++ app/src/main/java/com/example/apptender2/view/ui/fragments/MisdatosFragment.kt
@@ -1,12 +1,16 @@
 package com.example.apptender2.view.ui.fragments
 
+import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import androidx.fragment.app.Fragment
 import com.example.apptender2.R
-
 
 class misdatosFragment : Fragment() {
 
@@ -15,11 +19,34 @@ class misdatosFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_misdatos, container, false)
+        val view= inflater.inflate(R.layout.fragment_misdatos, container, false)
 
+        val btmcamara=view.findViewById<Button>(R.id.btmcamara)
+        btmcamara.setOnClickListener {
+            val intent= Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivityForResult(intent,123)
+        }
 
-
-
+        val btmgaleria=view.findViewById<Button>(R.id.btmgaleria)
+        btmgaleria.setOnClickListener {
+            val intent= Intent(Intent.ACTION_PICK)
+            intent.type = "image/*"
+            startActivityForResult(intent, 456)
+        }
+        return view
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        val imageView = view?.findViewById<ImageView>(R.id.fotoperfil)
+        if(requestCode==123){
+            var bitmap=data?.extras?.get("data")as Bitmap
+            imageView?.setImageBitmap(bitmap)
+        }else if(requestCode==456){
+            imageView?.setImageURI(data?.data)
+        }
+    }
+
+
 }

29e0d1c6972fe4370433cd7f5663c5f2fae6cb21
Scuervo07 <113921960+Scuervo07@users.noreply.github.com>
2022-11-15 20:40:23 -0500

    "Registro e inicio con firebase"

--- app/src/main/java/com/example/apptender2/view/ui/activities/LoginActivity.kt
+++ app/src/main/java/com/example/apptender2/view/ui/activities/LoginActivity.kt
@@ -1,30 +1,40 @@
 package com.example.apptender2.view.ui.activities
 
+import android.annotation.SuppressLint
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.apptender2.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity() {
 
         lateinit var iniciobutton: Button
         lateinit var registrobutton: Button
         lateinit var recuperarbutton: TextView
+        private lateinit var firebaseAuth: FirebaseAuth
+        @SuppressLint("MissingInflatedId")
+
         override fun onCreate(savedInstanceState: Bundle?) {
             super.onCreate(savedInstanceState)
             setContentView(R.layout.activity_login)
+            firebaseAuth = Firebase.auth
 
             iniciobutton = findViewById(R.id.BInicio)
             registrobutton = findViewById(R.id.BRegistro)
             recuperarbutton = findViewById(R.id.BRecuperar)
 
+            val correo = findViewById<EditText>(R.id.correologin)
+            val contrasena = findViewById<EditText>(R.id.contrasenalogin)
 
-            iniciobutton.setOnClickListener {
-                startActivity(Intent(this, HomeActivity::class.java ))
-
+            iniciobutton.setOnClickListener{
+                login(correo.text.toString(),contrasena.text.toString())
             }
 
             registrobutton.setOnClickListener {
@@ -33,6 +43,18 @@ class LoginActivity : AppCompatActivity() {
 
             recuperarbutton.setOnClickListener{
                 startActivity(Intent(this, RecuperarActivity::class.java))
+            }
+    }
+    private fun login (correo:String, contrasena:String){
+        firebaseAuth.signInWithEmailAndPassword(correo,contrasena)
+            .addOnCompleteListener(this) {
+                    task ->if (task.isSuccessful) {
+                    val user = firebaseAuth.currentUser
+                    Toast.makeText(baseContext,user?.uid.toString(),Toast.LENGTH_SHORT).show()
+                    startActivity(Intent(this, HomeActivity::class.java))
+                } else {
+                    Toast.makeText(baseContext, "Error en usuario y contraseña por favor validar", Toast.LENGTH_SHORT).show()
+                }
             }
     }
 }
